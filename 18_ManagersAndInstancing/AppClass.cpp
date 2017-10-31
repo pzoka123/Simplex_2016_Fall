7b93e2a28c7c55532941b82b26232e860ff04dd5
Tung Nguyen <nguyenhaitungg@gmail.com>
2017-10-31 10:49:48 -0400

    Render list

--- 18_ManagersAndInstancing/AppClass.cpp
+++ 18_ManagersAndInstancing/AppClass.cpp
@@ -23,7 +23,16 @@ void Application::InitVariables(void)
 	m_pMyMeshMngr = MyMeshManager::GetInstance();
 	m_pMyMeshMngr->SetCamera(m_pCamera);
 	
-	
+	m_pMesh = new MyMesh();
+	m_pMesh->GenerateCylinder(1.0f, 2.0f, 6, C_PURPLE);
+
+	for (uint i = 0; i < 5000; i++)
+	{
+		matrix4* pMatrix = new matrix4();
+		*pMatrix = glm::translate(IDENTITY_M4, vector3(i * 2.0f, 0.0f, 0.0f));
+		m_m4List.push_back(pMatrix);
+	}
+	//m_m4List.push_back(glm::translate(IDENTITY_M4, vector3(i * 2.0f, 0.0f, 0.0f)) * ToMatrix4(m_qArcBall));
 }
 void Application::Update(void)
 {
@@ -37,6 +46,7 @@ void Application::Update(void)
 	CameraRotation();
 
 	//Add objects to the Manager
+	/*
 	uint nCount = 0;
 	for (int j = -420; j < 420; j += 2)
 	{
@@ -47,11 +57,26 @@ void Application::Update(void)
 		}
 	}
 	m_pMeshMngr->Print("Objects: " + std::to_string(nCount) + "\n", C_BLUE);
+	*/
 }
 void Application::Display(void)
 {
 	//Clear the screen
 	ClearScreen();
+
+	/*
+	for (uint i = 0; i < 5000; i++)
+	{
+		m_pMesh->Render(m_pCamera, glm::translate(IDENTITY_M4, vector3(i * 2.0f, 0.0f, 0.0f)) * ToMatrix4(m_qArcBall));
+	}
+	*/
+
+	for (uint i = 0; i < 5000; i++)
+	{
+		*m_m4List[i] = glm::translate(IDENTITY_M4, vector3(i * 2.0f, 0.0f, 0.0f)) * ToMatrix4(m_qArcBall);
+	}
+
+	m_pMesh->Render(m_pCamera, m_m4List);
 
 	//Render the list of MyMeshManager
 	m_pMyMeshMngr->Render();
@@ -73,6 +98,11 @@ void Application::Display(void)
 }
 void Application::Release(void)
 {
+	for (uint i = 0; i < 5000; i++)
+	{
+		SafeDelete(m_m4List[i]);
+	}
+
 	//release the singleton
 	MyMeshManager::ReleaseInstance();
 
--- 18_ManagersAndInstancing/MyMesh.h
+++ 18_ManagersAndInstancing/MyMesh.h
@@ -113,6 +113,8 @@ public:
 	OUTPUT: ---
 	*/
 	void Render(MyCamera* a_pCamera, matrix4 a_mModel);
+
+	void Render(MyCamera* a_pCamera, std::vector<matrix4*> a_ToWorldList);
 	/*
 	USAGE: Will render this mesh a_ToWorlsList size times
 	ARGUMENTS:
